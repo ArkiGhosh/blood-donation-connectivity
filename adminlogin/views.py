25dddd698cf42c83d8b8223674625964313e1968
Ankesh Pandey <77636343+anvaishy@users.noreply.github.com>
2022-04-13 12:04:21 +0530

    Login Admin added without Frontend work

---
+++ adminlogin/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import mysql.connector as sql
+Uname = ''
+Pword = ''
+# Create your views here.
+def adminlogin(request):
+    global Uname,Pword
+    if request.method=="POST":
+        m = sql.connect(host="localhost",user="root",passwd="P@nky7050",database='DBMSproject')
+        cursor = m.cursor()
+        d = request.POST
+        for key,value in d.items():
+            if key == "username":
+                Uname = value
+            if key == "password":
+                Pword = value
+        
+        c = "select * from admin where Username = '{}' and Password = '{}'".format(Uname,Pword)
+        cursor.execute(c)
+        t = tuple(cursor.fetchall())
+        if t == ():
+            return render(request,'adminloginerror.html')
+        else:
+            return render(request,'admindashboard.html')
+
+    return render(request,'adminlogin.html')
---
+++ adminsignup/views.py
@@ -0,0 +1,23 @@
+from re import U
+from django.shortcuts import render
+import mysql.connector as sql
+Uname = ''
+Pword = ''
+# Create your views here.
+def adminsignup(request):
+    global Uname,Pword
+    if request.method=="POST":
+        m = sql.connect(host="localhost",user="root",passwd="P@nky7050",database='DBMSproject')
+        cursor = m.cursor()
+        d = request.POST
+        for key,value in d.items():
+            if key == "username":
+                Uname = value
+            if key == "password":
+                Pword = value
+        
+        c = "insert into admin Values('{}','{}')".format(Uname,Pword)
+        cursor.execute(c)
+        m.commit()
+    
+    return render(request,'adminsignup.html')
