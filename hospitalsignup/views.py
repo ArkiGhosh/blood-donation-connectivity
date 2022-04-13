9992416bdf4fe510f19e8fb8c97f2d56d7e0c892
Ankesh Pandey <77636343+anvaishy@users.noreply.github.com>
2022-04-13 18:48:35 +0530

    hospital login and signup done

--- hospitalsignup/views.py
+++ hospitalsignup/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+import mysql.connector as sql
+pin = ''
+name = ''
+contact = ''
+address = ''
 # Create your views here.
+def hospitalsignup(request):
+    if request.method=="POST":
+        m = sql.connect(host="localhost",user="root",passwd="P@nky7050",database='DBMSproject')
+        cursor = m.cursor()
+        d = request.POST
+        for key,value in d.items():
+            if key == "pin":
+                pin = value
+            if key == "name":
+                name = value
+            if key == "contact":
+                contact = value
+            if key == "address":
+                address = value
+        c = "insert into hospital Values('{}','{}','{}','{}')".format(pin,name,contact,address)
+        cursor.execute(c)
+        m.commit()
+    return render(request, 'hospitalsignup.html')
