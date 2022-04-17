0950f83d969f009f99731e2e1e19b8ae5b495d08
Ankesh Pandey <77636343+anvaishy@users.noreply.github.com>
2022-04-17 19:55:05 +0530

    Auto delete feature 42 days

--- hospitaldashboard/views.py
+++ hospitaldashboard/views.py
@@ -3,7 +3,7 @@ import mysql.connector as sql
 from django.contrib import messages
 # from hospitallogin.views import HOSPITAL_PIN
 import hospitallogin.views
-
+from datetime import datetime, timedelta
 
 hospital_pin = ''
 delpouch=''
@@ -15,11 +15,19 @@ def authenticate(var):
     # hospitaldashboard()
     return response
 
+
 def hospitaldashboard(request):
     # print(hospitallogin.views.HOSPITAL_PIN)
     global Uname,Pword
     m = sql.connect(host="localhost",user="root",passwd="P@nky7050",database='dbmsproject')
     cursor = m.cursor()
+    date42 = datetime.today() - timedelta(days = 42 )
+    c = "delete pouchbooking from pouchbooking inner join pouch on pouchbooking.PID = pouch.PouchID and pouch.DonationDate < '{}'".format(date42)
+    cursor.execute(c)
+    m.commit()
+    c="delete from pouch where DonationDate < '{}'".format(date42)
+    cursor.execute(c)
+    m.commit()
     d = request.POST
     for key,value in d.items():
         if key == "regpin":
@@ -29,7 +37,9 @@ def hospitaldashboard(request):
     c = "select * from pouch where HospitalPIN = '{}' order by DonationDate".format(hospital_pin)
     cursor.execute(c)
     t = tuple(cursor.fetchall())
+
     print(t)
+   
     if request.method=="POST":
         cursor = m.cursor()
         d = request.POST
@@ -37,6 +47,8 @@ def hospitaldashboard(request):
         for key,value in d.items():
             if key == "delpouch":
                 delpouch = value
+
+
         c="delete from pouchbooking where PID={}".format(delpouch)
         cursor.execute(c)
         m.commit()
