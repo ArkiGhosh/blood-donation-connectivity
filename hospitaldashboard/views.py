e5c37a2091547d63b859e3478487634a8a420493
Moksh Papneja <84846039+mokshpapneja@users.noreply.github.com>
2022-04-18 20:34:55 +0530

    Update views.py

--- hospitaldashboard/views.py
+++ hospitaldashboard/views.py
@@ -36,7 +36,7 @@ def authenticate(var):
 def hospitaldashboard(request):
     # print(hospitallogin.views.HOSPITAL_PIN)
     global Uname,Pword,delpouch,Pwt,Pht,Pcs,Page,Pvol,PG,PDD,PiB,PBG,PAdd,BookDate,Bookemail,BookPID,BookTime
-    m = sql.connect(host="localhost",user="root",passwd="pushpanjali",database='djangocrud')
+    m = sql.connect(host="localhost",user="root",passwd="P@nky7050",database='dbmsproject')
     cursor = m.cursor()
     date42 = datetime.today() - timedelta(days = 42 )
     c = "delete pouchbooking from pouchbooking inner join pouch on pouchbooking.PID = pouch.PouchID and pouch.DonationDate < '{}'".format(date42)
