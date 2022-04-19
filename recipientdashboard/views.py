e4ff8238aea54cc51d27d68624f1e3f8164c4fd7
Ankesh Pandey <77636343+anvaishy@users.noreply.github.com>
2022-04-19 15:22:20 +0530

    recipient dashboard booking logic and payment gateway left with frontend

--- recipientdashboard/views.py
+++ recipientdashboard/views.py
@@ -24,5 +24,18 @@ def recipientdashboard(request):
     global recipp
     recipp = tuple(cursor.fetchall()) # use for profile
     print(recipp)
+    #showing only non booked blood pouches
+    c = "select * from pouch inner join hospital on hospital.pin = pouch.hospitalpin where isbooked = 0"
+    cursor.execute(c)
+    unbookedpouch = tuple(cursor.fetchall())
+    print(unbookedpouch)
+    #when the pouch is booked we will have to change the isbooked to 1 here in backend
+
+
+
+    #here we have booked slots by the recipient only with all details needed (pouch details + hospital details) associated to the booking show just the essentials ones
+    d="select * from pouchbooking inner join pouch inner join hospital on hospital.pin = pouch.hospitalpin on pouch.pouchid=pouchbooking.pid where REmail ='{}'".format(email)
+    cursor.execute(d) 
+    bookings = tuple(cursor.fetchall())
     messages.success(request, 'You are signed in as '+email)
-    return render(request,'recipientdashboard.html')
+    return render(request,'recipientdashboard.html',{'ubp':unbookedpouch,'mb':bookings})
