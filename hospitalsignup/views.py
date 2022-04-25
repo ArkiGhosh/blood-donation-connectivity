109bb3c35ec716af57feea2c8087930a0d027ccc
Moksh Papneja <84846039+mokshpapneja@users.noreply.github.com>
2022-04-25 19:26:49 +0530

    Update views.py
    SMS recepient

--- hospitalsignup/views.py
+++ hospitalsignup/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import mysql.connector as sql
 from django.contrib import messages
-from twilio.rest import Client
+import requests
 pin = ''
 name = ''
 contact = ''
@@ -9,8 +9,8 @@ address = ''
 # Create your views here.
 def hospitalsignup(request):
     if request.method=="POST":
-        m = sql.connect(host="localhost",user="root",passwd="P@nky7050",database='DBMSproject')
         cursor = m.cursor()
+        m = sql.connect(host="localhost",user="root",passwd="P@nky7050",database='dbmsproject')
         d = request.POST
         for key,value in d.items():
             if key == "pin":
@@ -28,10 +28,25 @@ def hospitalsignup(request):
             c = "insert into hospital Values('{}','{}','{}','{}')".format(pin,name,contact,address)
             cursor.execute(c)
             m.commit()
-            # authtoken = "3f634b22f2d9c8024681ebd000d47760"
-            # authsid = "ACd6387bcde5a6fe3030c8afb480add54b"
-            # client = Client(authsid,authtoken)
-            # client.messages.create(to = "+91"+contact,from_ = "+19705174927",body="You have registered succesfully as Hospital")
+            #SMS
+
+
+
+            url = "https://www.fast2sms.com/dev/bulk"
+            numbers="{}".format(contact)
+            my_data = {
+                        'sender_id': 'Inspired Engine', 
+                        'message': 'Congratulations you are registered as a Hospital.', 
+                        'language': 'english',
+                        'route': 'p',
+                        'numbers': numbers  
+            }
+            headers = {
+                        'authorization': "KYQHzs86LewARVTbh9Ii0j7dcgaJSuDF4OMBo3qXEpWUmnykfCEcf7SVZNblrA8K9kGQgxqw6RnmD1zy",
+                        'Content-Type': "application/x-www-form-urlencoded",
+                        'Cache-Control': "no-cache"
+            }
+            response = requests.request("POST",url,data = my_data,headers = headers)
             
             messages.success(request, 'Registered successfully!')
         else:
